7495a7f56494d1c235cb6922a47f7477d253230b
David Hirtle <david.hirtle@pulseenergy.com>
2012-02-05 21:10:36 -0800

    If no messages, show some explanatory text

--- public/js/MessageList.js
+++ public/js/MessageList.js
@@ -1,5 +1,11 @@
 define(['ko', 'underscore', 'util', 'flavour'], function (ko, _, util, flavour) {
 
+	function Message(message, speaker, date) {
+		this.message = message;
+		this.speaker = speaker;
+		this.date = date;
+	}
+
 	function MessageList($element, socket) {
 		this.$element = $element;
 		this.messages = ko.observableArray();
@@ -20,9 +26,21 @@ define(['ko', 'underscore', 'util', 'flavour'], function (ko, _, util, flavour) 
 	MessageList.prototype.processHistory = function (events) {
 		this.reset(); // TODO: should properly find where left off
 
+		if (!events.length) {
+			this.showWelcome();
+			return;
+		}
+
 		_.each(events, function (event) {
 			this.append(event);
 		}, this);
+	};
+
+	MessageList.prototype.showWelcome = function () {
+		this.messages.push(
+			new Message('Welcome to Ω, a real-time issue tracker optimized for small teams.'),
+			new Message('This area shows recent activity and user messages so everyone on the team stays up-to-date.')
+		);
 	};
 
 	MessageList.prototype.reset = function (event) {
@@ -32,11 +50,8 @@ define(['ko', 'underscore', 'util', 'flavour'], function (ko, _, util, flavour) 
 	};
 
 	MessageList.prototype.append = function (event) {
-		this.messages.push({
-			speaker: event.speaker,
-			msg: flavour(event.type.name, util.addHtmlLinks(event.message)),
-			date: event.timestamp
-		});
+		var text = flavour(event.type.name, util.addHtmlLinks(event.message));
+		this.messages.push(new Message(text, event.speaker, event.timestamp));
 		scrollToBottom(this.$element.get(0));
 	};
 
